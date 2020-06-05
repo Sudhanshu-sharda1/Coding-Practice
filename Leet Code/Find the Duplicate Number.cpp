a4d2ac6045d28517cb12e487d5acf7e0142df424
Sudhanshu Sharda <sudhanshu.sharda@yahoo.com>
2020-06-05 21:44:07 +0530

    Leet Code #287

---
+++ Leet Code/Find the Duplicate Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        int tortoise = nums[0];
+        int hare = nums[0];
+        do{
+            hare = nums[nums[hare]];
+            tortoise = nums[tortoise];
+        }while(hare!=tortoise);
+        
+        tortoise = nums[0];
+        while(tortoise!=hare){
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+        return hare;
+    }
+};
