461abf313f7ca7ebcd6f67ec090e01c58df9e752
Sudhanshu Sharda <sudhanshu.sharda@yahoo.com>
2020-06-19 18:19:18 +0530

    Leet Code #643

---
+++ Leet Code/Maximum Average Subarray I.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        double sum = 0;
+        double avg = 0;
+        
+        for(int i=0; i<k; i++)
+            sum += nums[i];
+        avg = sum;
+        
+        for(int i=k; i<n; i++){
+            sum += nums[i] - nums[i-k];
+            avg = max(avg, sum);
+        }
+        return avg/k;
+    }
+};
